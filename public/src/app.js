84863892843cf90dfef061ba0b0a6bfa5191cde6
Richard Hong <rich.hong@gmail.com>
2016-08-27 17:12:43 -0700

    fixed component positioning

--- public/src/app.js
+++ public/src/app.js
@@ -15,40 +15,43 @@ $(document).ready(function(){
   // init initializes app with Jquery event listeners
   function init() {
     $('#search-form').on('submit', function(e){
-
       e.preventDefault();
-
       searchSpotify();
     });
   }
 
   // searchSpotify makes an ajax request to Spotify search API
   function searchSpotify () {
+    var query = $('#search-text').val();
+    if (!query) {
+      renderNoQueryError();
+    } else {
+      var options = {
+        q: query,
+        type: 'artist'
+      };
 
-    var query = $('#search-text').val() || 'kamaiyah';
-
-    var options = {
-      q: query,
-      type: 'artist'
-    };
-
-    $.get('https://api.spotify.com/v1/search', options, function(data, response){
-        state.artists = data.artists.items;
-        renderArtists(state.artists);
-      })
+      $.get('https://api.spotify.com/v1/search', options, function(data, response){
+        if (data.artists.items.length === 0){
+          renderNoResultsError();
+        } else {
+          state.artists = data.artists.items;
+          renderArtists(state.artists);
+        }
+        })
+      $('#search-text').val('')
+    }
   };
 
   // renderArtists appends search results from Spotify API to unordered list, #artists
   function renderArtists (artists) {
     $('#artists-list').empty();
-
     for(var i = 0; i < artists.length; i++ ){
       $('#artists-list').append('<li class="artist" data-artist-id="' + artists[i].id + '">' + artists[i].name + '</li>');
     }
 
     // onClick event listener when artist is clicked to select current artist
     $('.artist').on('click', function() {
-
       state.artistId = $(this).data().artistId;
 
       $.get('https://api.spotify.com/v1/artists/' + state.artistId, function(data, response){
@@ -56,7 +59,6 @@ $(document).ready(function(){
         renderArtistInfo(state.artist);
 
         $.get('https://api.spotify.com/v1/artists/' + state.artistId + '/albums', function(data, response){
-          console.log(data)
           state.albums = data.items;
           renderAlbums(state.albums);
         })
@@ -66,28 +68,34 @@ $(document).ready(function(){
 
   // renderArtistInfo appends selected artist info to div, #artistInfo
   function renderArtistInfo (artist) {
-
     $('#artist-info').empty();
-
-    $('#artist-info').append('<span>' + artist.name + '</span><br /><img class="avatar" src="' + artist.images[0].url + '"/><span>Followers: ' + artist.followers.total + '</span><br/><span>Popularity: ' + artist.popularity + '</span><br/>')
+    var artwork;
+    artist.images.length ? artwork = artist.images[0].url : artwork = 'http://i.imgur.com/nszu54A.jpg'
+    $('#artist-info').append('<span class="artist-name">' + artist.name + '</span><br /><img class="avatar" src="' + artwork + '"/>')
   }
   function renderAlbums (albums) {
-
     $('#artist-album-list').empty();
-
     for (var j = 0; j < albums.length; j++) {
       $('#artist-album-list').append('<li class=album data-uri="' + albums[j].uri +'">' + albums[j].name + '</li>')
     }
 
     $('.album').on('click', function(){
-      var albumUri = $(this).data().uri
-      renderPlayer(albumUri)
+      var uri = $(this).data().uri
+      renderPlayer(uri)
     })
 
   }
   function renderPlayer (uri) {
     $('#player-main-container').empty();
     $('#player-main-container').append('<iframe src="https://embed.spotify.com/?uri='+uri+'" width="100%" height="380" frameborder="0" allowtransparency="true"></iframe>')
+  }
+  function renderNoResultsError() {
+    $('#artists-list').empty();
+    $('#artists-list').append('<li class="error">No search results.</li>')
+  }
+  function renderNoQueryError() {
+    $('#artists-list').empty();
+    $('#artists-list').append('<li class="error">Search cannot be empty.</li>')
   }
 
   init();
