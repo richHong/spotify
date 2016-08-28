638d4bfdafe68ee4a9cd535b6b97659b9c47fcff
Richard Hong <rich.hong@gmail.com>
2016-08-27 19:04:03 -0700

    removed duplicate names

--- public/src/app.js
+++ public/src/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     artist: null,
     artists: null,
     albums: null
-  }
+  };
 
   /******** Helper Functions *********/
 
@@ -38,14 +38,15 @@ $(document).ready(function(){
           state.artists = data.artists.items;
           renderArtists(state.artists);
         }
-        })
-      $('#search-text').val('')
+      });
+      $('#search-text').val('');
     }
   };
 
   // renderArtists appends search results from Spotify API to unordered list, #artists
   function renderArtists (artists) {
     $('#artists-list').empty();
+    artists = _.uniq(artists, 'name');
     for(var i = 0; i < artists.length; i++ ){
       $('#artists-list').append('<li class="artist" data-artist-id="' + artists[i].id + '">' + artists[i].name + '</li>');
     }
@@ -61,7 +62,7 @@ $(document).ready(function(){
         $.get('https://api.spotify.com/v1/artists/' + state.artistId + '/albums', function(data, response){
           state.albums = data.items;
           renderAlbums(state.albums);
-        })
+        });
       });
     });
   }
@@ -70,19 +71,20 @@ $(document).ready(function(){
   function renderArtistInfo (artist) {
     $('#artist-info').empty();
     var artwork;
-    artist.images.length ? artwork = artist.images[0].url : artwork = 'http://i.imgur.com/nszu54A.jpg'
-    $('#artist-info').append('<span class="artist-name">' + artist.name + '</span><br /><img class="avatar" src="' + artwork + '"/>')
+    artist.images.length ? artwork = artist.images[0].url : artwork = 'http://i.imgur.com/nszu54A.jpg';
+    $('#artist-info').append('<span class="artist-name">' + artist.name + '</span><br /><img class="avatar" src="' + artwork + '"/>');
   }
   function renderAlbums (albums) {
     $('#artist-album-list').empty();
+    albums = _.uniq(albums, 'name');
     for (var j = 0; j < albums.length; j++) {
-      $('#artist-album-list').append('<li class=album data-uri="' + albums[j].uri +'">' + albums[j].name + '</li>')
+      $('#artist-album-list').append('<li class=album data-uri="' + albums[j].uri +'">' + albums[j].name + '</li>');
     }
 
     $('.album').on('click', function(){
-      var uri = $(this).data().uri
-      renderPlayer(uri)
-    })
+      var uri = $(this).data().uri;
+      renderPlayer(uri);
+    });
 
   }
   function renderPlayer (uri) {
@@ -91,11 +93,11 @@ $(document).ready(function(){
   }
   function renderNoResultsError() {
     $('#artists-list').empty();
-    $('#artists-list').append('<li class="error">No search results.</li>')
+    $('#artists-list').append('<li class="error">No search results.</li>');
   }
   function renderNoQueryError() {
     $('#artists-list').empty();
-    $('#artists-list').append('<li class="error">Search cannot be empty.</li>')
+    $('#artists-list').append('<li class="error">Search cannot be empty.</li>');
   }
 
   init();
